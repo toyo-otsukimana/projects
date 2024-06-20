591033fbc985417e2d42a3e121122aeb75d335a4
Tonos4ki <168062354+Tonos4ki@users.noreply.github.com>
2024-06-20 14:53:10 +0900

    ヘッダーjs機能追加

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,10 @@
-$(function(){
-  $('.nav-list li a').each(function(){
+$(document).ready(function() {
+  $('.nav-list li a').each(function() {
       var $href = $(this).attr('href');
-      if(location.href.match($href)) {
-      $(this).addClass('active');
+      if (location.pathname.endsWith($href)) {
+          $(this).addClass('active');
       } else {
-      $(this).removeClass('active');
+          $(this).removeClass('active');
       }
   });
 });
