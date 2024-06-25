1a64ef97f4fc0bc4f78b36ddc193ca718db2a942
Tonos4ki <168062354+Tonos4ki@users.noreply.github.com>
2024-06-25 17:23:30 +0900

    レスポンシブ化

--- js/code.js
+++ js/code.js
@@ -1,23 +1,24 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
   const codeBlock = document.getElementById('codeBlock');
-  const codeString = `&lt;!DOCTYPE html&gt;
+  const codeString = 
+`&lt;!DOCTYPE html&gt;
 &lt;html lang="ja"&gt;
 &lt;head&gt;
-  &lt;meta charset="UTF-8"&gt;
-  &lt;meta name="viewport" content="width=device-width, initial-scale=1.0"&gt;
-  &lt;title&gt;ページのタイトル&lt;/title&gt;
+    &lt;meta charset="UTF-8"&gt;
+    &lt;meta name="viewport" content="width=device-width, initial-scale=1.0"&gt;
+    &lt;title&gt;ページのタイトル&lt;/title&gt;
 &lt;/head&gt;
 &lt;body&gt;
-  &lt;header&gt;
-      &lt;h1&gt;見出し&lt;/h1&gt;
-  &lt;/header&gt;
-  &lt;main&gt;
-      &lt;p&gt;ここに本文を書きます。&lt;/p&gt;
-  &lt;/main&gt;
-  &lt;footer&gt;
-      &lt;p&gt;フッターの内容&lt;/p&gt;
-  &lt;/footer&gt;
+    &lt;header&gt;
+        &lt;h1&gt;見出し&lt;/h1&gt;
+    &lt;/header&gt;
+    &lt;main&gt;
+        &lt;p&gt;ここに本文を書きます。&lt;/p&gt;
+    &lt;/main&gt;
+    &lt;footer&gt;
+        &lt;p&gt;フッターの内容&lt;/p&gt;
+    &lt;/footer&gt;
 &lt;/body&gt;
 &lt;/html&gt;`;
 
